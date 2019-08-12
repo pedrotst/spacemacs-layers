f07cb5a859fbaa52fb3e51da95926f8530991753
Pedro Abreu <abreu223@hotmail.com>
2019-08-12 11:20:55 -0500

    Cedille mode

--- cedille/packages.el
+++ cedille/packages.el
@@ -36,7 +36,71 @@
 (defun cedille/init-cedille-mode ()
   (use-package cedille-mode
     :init
-    (require 'cedille-mode)
+    (progn
+      (require 'cedille-mode)
+      (spacemacs/set-leader-keys-for-major-mode 'cedille-mode
+        "w" 'cedille-start-navigation
+        "f" 'cedille-mode-select-next
+        "F" 'cedille-mode-select-next-alt
+        "b" 'cedille-mode-select-previous
+        "B" 'cedille-mode-select-previous-alt
+        "p" 'cedille-mode-select-parent
+        "n" 'cedille-mode-select-first-child
+        "H" 'cedille-mode-interactive-highlight
+        "g" 'se-mode-clear-selected
+        "q" 'cedille-mode-quit
+        "Q" 'cedille-mode-quit-keep-mark
+        "M-s" 'cedille-mode-quit
+        "C-g" 'cedille-mode-quit
+        "e" 'cedille-mode-select-last
+        "a" 'cedille-mode-select-first
+        "i" (make-cedille-mode-buffer (cedille-mode-inspect-buffer) lambda cedille-mode-inspect nil t)
+        "I" (make-cedille-mode-buffer (cedille-mode-inspect-buffer) lambda cedille-mode-inspect t t)
+        "j" 'cedille-mode-jump
+        "=" 'cedille-mode-replace-occurrences
+        "." (make-cedille-mode-history-navigate t nil)
+        "," (make-cedille-mode-history-navigate nil nil)
+        "<" (make-cedille-mode-history-navigate nil t)
+        ">" (make-cedille-mode-history-navigate t t)
+        "r" 'cedille-mode-select-next-error
+        "R" 'cedille-mode-select-previous-error
+        "t" 'cedille-mode-select-first-error-in-file
+        "T" 'cedille-mode-select-last-error-in-file
+        "c" (make-cedille-mode-buffer (cedille-mode-context-buffer) lambda cedille-context-view-mode nil t)
+        "C" (make-cedille-mode-buffer (cedille-mode-context-buffer) lambda cedille-context-view-mode t t)
+        "s" (make-cedille-mode-buffer (cedille-mode-summary-buffer) cedille-mode-summary cedille-summary-view-mode nil nil)
+        "S" (make-cedille-mode-buffer (cedille-mode-summary-buffer) cedille-mode-summary cedille-summary-view-mode t nil)
+        "#" 'cedille-mode-highlight-occurrences
+        "m" (make-cedille-mode-buffer (cedille-mode-meta-vars-buffer) lambda cedille-meta-vars-mode nil t)
+        "M" (make-cedille-mode-buffer (cedille-mode-meta-vars-buffer) lambda cedille-meta-vars-mode t t)
+        "K" 'cedille-mode-restart-backend
+        "h" (make-cedille-mode-info-display-page nil)
+        "E" 'cedille-mode-elaborate
+        "$" (make-cedille-mode-customize "cedille")
+        "1" 'delete-other-windows
+        "P" (lambda () (interactive) (message "se-mode-selected: %s" (se-mode-selected)))
+        "?" 'cedille-mode-backend-debug
+        "x" 'cedille-mode-scratch-toggle
+        "X" (lambda () (interactive) (cedille-mode-scratch-toggle t))
+        "+" (make-cedille-mode-resize-current-window 1)
+        "-" (make-cedille-mode-resize-current-window -1)
+        "=" 'cedille-mode-unlock-current-window-size
+        "C-x 2" 'cedille-mode-split-window
+        "M-c" 'cedille-mode-scratch-copy-span
+        "C-h 1" 'cedille-mode-highlight-default
+        "C-h 2" 'cedille-mode-highlight-language-level
+        "C-h 3" 'cedille-mode-highlight-checking-mode
+        ; Interactive commands
+        "C-i h" 'cedille-mode-head-normalize
+        "C-i n" 'cedille-mode-normalize
+        "C-i u" 'cedille-mode-single-reduction
+        "C-i e" 'cedille-mode-erase
+        "C-i b" 'cedille-mode-br
+        "C-i B" 'cedille-mode-br-node
+        "C-i t" 'cedille-mode-br-type
+        "C-i r" 'cedille-mode-inspect-clear
+        "C-i R" 'cedille-mode-inspect-clear-all
+        ))
   ))
 
 ;;; packages.el ends here
